eda628e5062402fbbde3359ac33f06a7bdfb8ea5
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-12-03 17:41:33 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+            System.out.println("Random Number: " + item);
+    }
+        for (int i = 0; i < 10; i++) {
+
+public class RandomDataGenerator {
+        for (int item : data) {
+    public static void main(String[] args) {
+
+        int[] data = new int[10];
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }def generate_random_data():
+def main():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
+    for item in data:
+
+
+import random
+
+
+    main()
+    data = generate_random_data()
