f21858dee27d36be13bec0d962b548be62012b1f
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-12-11 12:30:52 +0200

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,67 @@
-    data = generate_random_data()
-    main()
-import random
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+}
+        }
+    }
+
+        for (int item : data) {
+    public static void main(String[] args) {
+
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+public class RandomDataGenerator {    main()
 def main():
+import random
 if __name__ == "__main__":
 
-
+    for item in data:
 
         print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
 
     return data
-    for item in data:
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]    data = [random.randint(1, 100) for _ in range(10)]
     data = generate_random_data()
-import random
-if __name__ == "__main__":
+main();
+    const data = generateRandomData();
+
+    const data = [];
+    }
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+}
+function main() {
+
+    });
+        console.log("Random Number: " + item);
+function generateRandomData() {
+
+    return data;
+}
+        data.push(Math.floor(Math.random() * 100) + 1);    data = [random.randint(1, 100) for _ in range(10)]
     for item in data:
-
-def generate_random_data():
-        print(f"Random Number: {item}")
+    data = generate_random_data()
     return data
-
-
-
-    main()
 def main():
+
+import random
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+    main()
+if __name__ == "__main__":
+
+            System.out.println("Random Number: " + item);
+}
+        int[] data = new int[10];
+
+        for (int item : data) {
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+    }
+        }
